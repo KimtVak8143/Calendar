c94c954dfa2310e657570ea794c6799d51e85a01
KimtVak8143 <mk8143@srmist.edu.in>
2021-08-11 20:47:51 +0530

    changes 1

--- Calendar/new.c
+++ Calendar/new.c
@@ -3,22 +3,31 @@
 #include<string.h>
 #include<ctype.h>
 
-void diff(int,int,int); // to calculate the difference between 2 dates - day wise, month wise, year wise
+//void diff(int,int,int); // to calculate the difference between 2 dates - day wise, month wise, year wise
 void show(); //to show the calendar dates
 
-typedef struct date
+typedef struct D
 {   int day;
     char month[10];
     int year;
 }date;
 
 void main()
-{
+{   printf("Hello\n");
+    show();
 
 }
 
-
-void diff(int a,int b,int c,int );
-{   
-
+void show()
+{   int i;
+    char *days[10]={"Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"};
+    for(i=0;i<7;i++)
+    {   printf("%s ",days[i]);
+    }
+   // printf("\n");
+    for(i=1;i<=31;i++)
+    {   if(i%7==1)
+            printf("\n");
+        printf("%d  ",i);
+    }
 }
