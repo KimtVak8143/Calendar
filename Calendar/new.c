b0defcf4ca5cdfb5070e6d8fdc932cba2b48f934
KimtVak8143 <mk8143@srmist.edu.in>
2021-08-11 19:20:20 +0530

    added files

---
+++ Calendar/new.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<math.h>
+#include<string.h>
+#include<ctype.h>
+
+void diff(int,int,int); // to calculate the difference between 2 dates - day wise, month wise, year wise
+void show(); //to show the calendar dates
+
+typedef struct date
+{   int day;
+    char month[10];
+    int year;
+}date;
+
+void main()
+{
+
+}
+
+
+void diff(int a,int b,int c,int );
+{   
+
+}
